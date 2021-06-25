955cd8d73b95c9cc2c0559055b3e2a83cb56593f
BrenoXDMoon <brenogabriel19@gmail.com>
2021-06-25 13:32:12 -0300

    Projeto

---
+++ ChopperHouse/src/main/java/br/com/fatec/ChopperHouse/ChopperHouseApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.ChopperHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopperHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChopperHouseApplication.class, args);
+	}
+
+}
---
+++ ChopperHouse/src/test/java/br/com/fatec/ChopperHouse/ChopperHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.ChopperHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChopperHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
