7a4d0b488b029c122b46f05db5c03c7f6168cbcf
BrenoXDMoon <brenogabriel19@gmail.com>
2022-02-27 14:09:27 -0300

    Update ClienteMapper.java

--- src/main/java/br/com/fatec/ChopperHouseGames/inbound/facade/mapper/ClienteMapper.java
+++ src/main/java/br/com/fatec/ChopperHouseGames/inbound/facade/mapper/ClienteMapper.java
@@ -3,14 +3,19 @@ package br.com.fatec.ChopperHouseGames.inbound.facade.mapper;
 import br.com.fatec.ChopperHouseGames.core.domain.Cliente;
 import br.com.fatec.ChopperHouseGames.inbound.facade.dto.ClienteDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ClienteMapper {
 
+    @Mapping(source = "senha", target = "senha.senha")
+    @Mapping(source = "confirmaSenha", target = "senha.confirmaSenha")
     Cliente toCliente(ClienteDTO dto);
 
+    @Mapping(source = "senha.senha", target = "senha")
+    @Mapping(source = "senha.confirmaSenha", target = "confirmaSenha")
     ClienteDTO toClienteDTO(Cliente cliente);
 
     List<ClienteDTO> toListDTO(List<Cliente> listar);
