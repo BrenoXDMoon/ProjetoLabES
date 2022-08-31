5d1e85c6b2e6f33dfb18f3375b5fb182e08deab8
BrenoXDMoon <brenogabriel19@gmail.com>
2022-08-30 21:47:30 -0300

    Refactor no fluxo de dados para JPARepositories

--- src/main/java/br/com/fatec/ChopperHouseGames/core/repository/ClienteRepository.java
+++ src/main/java/br/com/fatec/ChopperHouseGames/core/repository/ClienteRepository.java
@@ -2,11 +2,11 @@ package br.com.fatec.ChopperHouseGames.core.repository;
 
 import br.com.fatec.ChopperHouseGames.core.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     Optional<Cliente> findByEmail(String email);
+
 }
--- src/main/java/br/com/fatec/ChopperHouseGames/outbound/repository/ClienteJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fatec.ChopperHouseGames.outbound.repository;
-
-import br.com.fatec.ChopperHouseGames.core.domain.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteJpaRepository extends JpaRepository<Cliente, Long> {
-
-}
